c7d4cedc1034145163d9803bb502cb56b7cb2376
Deennn <abideenlawal70@gmail.com>
2022-07-11 22:47:59 +0100

    made some progress

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'This is a description',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frecipes-for-all.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fdelish-antipasto-potato-salad-still001-1530191842-e1588452642459.jpg&f=1&nofb=1'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredients } from '../shared/ingredients.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apples', 5),
+    new Ingredients('Tomato', 10),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
